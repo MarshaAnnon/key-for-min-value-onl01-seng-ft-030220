479c2c699be322ceac85738996590a4d3319daab
Marsha Annon <marshaannon@gmail.com>
2020-03-08 04:10:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-styles = {t_shirts: 30, dresses: 59, vest: 24, coats: 95}
-def key_for_min_value(styles)
-  styles.collect do |type, price|
-    return key_for_min_value(styles)
+stock = {
+  t_shirts: 30,
+  dresses: 59,
+  tanks: 24,
+  coats: 95
+}
+
+def stock_levels(amount)
+    lowest = amount.find {|s| s[:apr] < amount[:apr] }
 end
 
 end
