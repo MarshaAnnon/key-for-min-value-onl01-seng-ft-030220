2bfe19f13f3e054ce9c3180c967951984ad30f91
Marsha Annon <marshaannon@gmail.com>
2020-03-08 05:12:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(hash)
   lowest_key []
 
-  name_hash.collect do |key, value|
+  hash.collect do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
