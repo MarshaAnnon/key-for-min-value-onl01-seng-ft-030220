ecf008006d293a7382fb4e0d5d96144b99daa25d
Marsha Annon <marshaannon@gmail.com>
2020-03-08 04:52:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  
+  smallest_name = nil
+  smallest = 1000000
+
+  hash.each do |key, value|
+    if value <= smallest
+      smallest = value
+      smallest_name = key
+    end
+  end
+  smallest_name
+end
 end
 #restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
 #restaurant_menu.each do | item, price |
