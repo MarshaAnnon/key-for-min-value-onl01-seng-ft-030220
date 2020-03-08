d94abd2c1b16de8b62d1392a1982dec71a3c3112
Marsha Annon <marshaannon@gmail.com>
2020-03-08 03:57:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
-
+styles = {t_shirts: 30, dresses: 59, vest: 24, coats: 95}
+def key_for_min_value(styles)
+  styles.collect do |type, price|
+end
+key_for_min_value(styles)
 end
